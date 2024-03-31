1814f8a7200ad364e01b753e5a9fb55d41895557
marsel945 <rewoherson519@gmail.com>
2024-03-31 21:42:37 +0700

    add: penilaian & user factory

--- app/Models/Penilaian.php
+++ app/Models/Penilaian.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Penilaian extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $table = 'penilaian';
+    protected $primaryKey = 'penilaian_id';
+    public $timestamps = false;
 }
