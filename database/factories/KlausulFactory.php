1cd58f8faa951987dd5f34a523d7ecb1924c7779
marsel945 <rewoherson519@gmail.com>
2024-03-31 21:39:49 +0700

    add: klausul factory

---
+++ database/factories/KlausulFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Klausul>
+ */
+class KlausulFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'klausul_name' => fake()->sentence()
+        ];
+    }
+}
