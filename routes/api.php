e34b9e46c7b1a1a11ef3157c5d0b8f0ef4701321
marsel945 <rewoherson519@gmail.com>
2024-04-02 13:42:29 +0700

    Edit function name

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PenilaianController;
-   
+
 
 // Rute untuk autentikasi
-Route::prefix('auth')->group(function () {   
+Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    
+
     // Rute di bawah ini memerlukan autentikasi
     Route::middleware('auth.jwt')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
@@ -20,8 +20,8 @@ Route::prefix('auth')->group(function () {
 
 // Rute untuk penilaian memerlukan autentikasi
 Route::middleware('auth.jwt')->group(function () {
-    Route::get('penilaian', [PenilaianController::class , 'index']);
-    Route::get('/rekomendasi', [RekomendasiController::class , 'index']);
+    Route::get('/penilaians', [PenilaianController::class, 'get_penilaian']);
+    Route::get('/rekomendasi', [RekomendasiController::class, 'index']);
     // Definisikan rute lainnya yang memerlukan autentikasi di sini
 });
-Route::get('klausul', [PenilaianController::class , 'klausul']);
+Route::get('klausul', [PenilaianController::class, 'klausul']);
