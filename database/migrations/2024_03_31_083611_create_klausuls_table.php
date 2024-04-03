f3b7c0620fdcdc20a0ead1ac3861867470f9f581
marsel945 <rewoherson519@gmail.com>
2024-04-03 09:59:21 +0700

    remove item attribute

--- database/migrations/2024_03_31_083611_create_klausuls_table.php
+++ database/migrations/2024_03_31_083611_create_klausuls_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('klausul', function (Blueprint $table) {
             $table->uuid('klausul_id')->primary();
             $table->string('klausul_name');
-            $table->json('item');
         });
     }
 
