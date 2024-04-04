7b8fca33ea4c63ec8fbd2de3a6a64d07bb7a6f31
marsel945 <rewoherson519@gmail.com>
2024-04-04 09:24:45 +0700

    Add Seeder

--- database/seeders/PenilaianDataSeeder.php
+++ database/seeders/PenilaianDataSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Departement;
 use App\Models\Klausul;
+use App\Models\KlausulItem;
+use App\Models\Laporan;
 use App\Models\Penilaian;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,73 +19,16 @@ class PenilaianDataSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        $departement = Departement::factory()->create();
-        $user = \App\Models\User::factory()->create([
-            'departements_id' => $departement->departements_id
-        ]);
-        $laporan = \App\Models\Laporan::factory()->create([
-            'user_id' => $user->id,
-            'departements_id' => $user->departements_id
-        ]);
-        Penilaian::insert([
-            [
-                'penilaian_id' => \Str::uuid(),
-                'laporan_id' => $laporan->laporan_id,
-                'penilaian_target' => 1,
-                'penilaian_aktual' => 1,
-                'penilaian_keterangan' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe ex soluta, quisquam eos reprehenderit eveniet esse obcaecati amet cumque, debitis ut provident illo libero expedita explicabo. A, eos! A.',
-                'disetujui' => 1,
-                'klausul_id' => Klausul::skip(5)->first()->klausul_id
-            ],
+    public function run(){
 
-            [
-                'penilaian_id' => \Str::uuid(),
-                'laporan_id' => $laporan->laporan_id,
-                'penilaian_target' => 1,
-                'penilaian_aktual' => 1,
-                'penilaian_keterangan' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe ex soluta, quisquam eos reprehenderit eveniet esse obcaecati amet cumque, debitis ut provident illo libero expedita explicabo. A, eos! A.',
-                'disetujui' => 0,
-                'klausul_id' => Klausul::skip(4)->first()->klausul_id
-            ],
-            [
-                'penilaian_id' => \Str::uuid(),
-                'laporan_id' => $laporan->laporan_id,
-                'penilaian_target' => 1,
-                'penilaian_aktual' => 1,
-                'penilaian_keterangan' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe ex soluta, quisquam eos reprehenderit eveniet esse obcaecati amet cumque, debitis ut provident illo libero expedita explicabo. A, eos! A.',
-                'disetujui' => 1,
-                'klausul_id' => Klausul::skip(3)->first()->klausul_id
-            ],
-            [
-                'penilaian_id' => \Str::uuid(),
-                'laporan_id' => $laporan->laporan_id,
-                'penilaian_target' => 1,
-                'penilaian_aktual' => 1,
-                'penilaian_keterangan' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe ex soluta, quisquam eos reprehenderit eveniet esse obcaecati amet cumque, debitis ut provident illo libero expedita explicabo. A, eos! A.',
-                'disetujui' => 1,
-                'klausul_id' => Klausul::skip(2)->first()->klausul_id
-            ],
+        $laporan = Laporan::first();
+        $klausul_items = KlausulItem::all();
 
-            [
-                'penilaian_id' => \Str::uuid(),
+        foreach($klausul_items as $item){
+            Penilaian::create([
                 'laporan_id' => $laporan->laporan_id,
-                'penilaian_target' => 1,
-                'penilaian_aktual' => 1,
-                'penilaian_keterangan' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe ex soluta, quisquam eos reprehenderit eveniet esse obcaecati amet cumque, debitis ut provident illo libero expedita explicabo. A, eos! A.',
-                'disetujui' => 1,
-                'klausul_id' => Klausul::skip(1)->first()->klausul_id
-            ],
-            [
-                'penilaian_id' => \Str::uuid(),
-                'laporan_id' => $laporan->laporan_id,
-                'penilaian_target' => 1,
-                'penilaian_aktual' => 1,
-                'penilaian_keterangan' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe ex soluta, quisquam eos reprehenderit eveniet esse obcaecati amet cumque, debitis ut provident illo libero expedita explicabo. A, eos! A.',
-                'disetujui' => 1,
-                'klausul_id' => Klausul::first()->klausul_id
-            ],
-        ]);
+                'klausul_item_id' => $item->id
+            ]);
+        }
     }
 }
