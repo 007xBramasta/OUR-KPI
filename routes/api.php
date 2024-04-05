25c4f08cd68aa4fc68551f8cc27ee53848b73cf9
007xBramasta <bramasta026x@gmail.com>
2024-04-05 13:27:59 +0700

    edit api

--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,8 @@ Route::middleware('auth.jwt')->group(function () {
     Route::get('/penilaians', [PenilaianController::class, 'get_penilaian']);
     Route::patch('/penilaians/{penilaianId}/klausuls/{klausulId}', [PenilaianController::class, 'update_penilaian']);
     Route::get('/rekomendasi', [RekomendasiController::class, 'index']);
-    Route::get('/departements', [DepartementController::class, 'get_departement']);
     Route::get('/laporan', [LaporanController::class, 'showMonthlyReport']);
     // Definisikan rute lainnya yang memerlukan autentikasi di sini
 });
 Route::get('klausul', [PenilaianController::class, 'klausul']);
+Route::get('/departements', [DepartementController::class, 'get_departement']);
