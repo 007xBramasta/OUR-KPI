a6c811640d8bb9d421e3ac88ff54499db3982b7b
007xBramasta <bramasta026x@gmail.com>
2024-04-05 15:24:42 +0700

    add logs to laporan service

--- app/Http/Services/LaporanService.php
+++ app/Http/Services/LaporanService.php
@@ -2,29 +2,38 @@
 
 namespace App\Http\Services;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class LaporanService
 {
     public function getMonthlyReport($month)
     {
+        DB::enableQueryLog(); // Enable query log
+
         if (strlen($month) === 1) {
-            return DB::table('laporan')
-                ->whereRaw("SUBSTRING(created_at, 6, 2) = ?", [$month])
-                ->get();
+    
+            $query = DB::table('laporan')
+                ->whereRaw("SUBSTRING(created_at, 6, 2) = ?", [$month]);
+
+            Log::info($query->toSql(), $query->getBindings()); 
+
+            return $query->get();
         }
 
         if (strlen($month) > 1) {
-            // Mendapatkan array bulan dari input yang diberikan
+            
             $months = explode(',', $month);
 
-            // Menggunakan OR untuk mencari laporan dengan bulan yang sesuai dengan array bulan yang diberikan
-            return DB::table('laporan')
+            $query = DB::table('laporan')
                 ->where(function ($query) use ($months) {
                     foreach ($months as $month) {
                         $query->orWhereRaw("SUBSTRING(created_at, 6, 2) = ?", [$month]);
                     }
-                })
-                ->get();
+                });
+
+            Log::info($query->toSql(), $query->getBindings()); // Log the SQL query
+
+            return $query->get();
         }
     }
 }
