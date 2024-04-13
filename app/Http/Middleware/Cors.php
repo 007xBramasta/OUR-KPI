80d102b3b9a5148dc61bedfa0730fa6eeac7d30a
007xBramasta <bramasta026x@gmail.com>
2024-04-13 09:52:42 +0700

    update cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,14 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://192.168.18.10:5173')
+            ->header('Access-Control-Allow-Origin', [
+                'http:localhost:5173',
+                'http://127.0.0.1:5173',
+                'http://192.168.56.1:5173',
+                'http://192.168.137.1:5173',
+                'http://192.168.18.10:5173',
+                'https://our-kpi-production.vercel.app'
+            ])
             ->header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
             ->header('Access-Control-Allow-Credentials', 'true');
