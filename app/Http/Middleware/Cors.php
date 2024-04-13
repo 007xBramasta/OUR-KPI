9a345e7a4965f1cad19d96deee586c815f2c842c
007xBramasta <bramasta026x@gmail.com>
2024-04-13 09:14:49 +0700

    fix cors origin

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,13 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+        $response = $next($request)
+            ->header('Access-Control-Allow-Origin', 'http://192.168.18.10:5173')
             ->header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        $response->header('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
     }
 }
