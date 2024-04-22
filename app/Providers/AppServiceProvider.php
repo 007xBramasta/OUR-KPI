fdb88b4e8c48600fe2ebb36c6c9f146b665b4f6d
007xBramasta <bramasta026x@gmail.com>
2024-04-22 13:30:05 +0700

    comment listen log query sql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,16 +26,16 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        DB::listen(function ($query) {
-            Log::info(
-                $query->sql,
-                [
-                    'bindings' => $query->bindings,
-                    'time' => $query->time
-                ]
-            );
-        });
-    }
+    // public function boot()
+    // {
+    //     DB::listen(function ($query) {
+    //         Log::info(
+    //             $query->sql,
+    //             [
+    //                 'bindings' => $query->bindings,
+    //                 'time' => $query->time
+    //             ]
+    //         );
+    //     });
+    // }
 }
